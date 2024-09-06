31462c0e502e6f7484cc859127a4d908ea1a2a52
Garudev <144961613+Begarudev@users.noreply.github.com>
2024-09-06 07:13:45 +0530

    Refactor widget layout in curved_navigation_bar.dart

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -1,228 +1,144 @@
 import 'dart:math';
 import 'dart:ui';
 
-import 'package:curved_navigation_bar/src/nav_custom_clipper.dart';
 import 'package:flutter/material.dart';
 
-import 'src/nav_button.dart';
-import 'src/nav_custom_painter.dart';
+class NavCustomPainter extends CustomPainter {
+  late double loc;
+  late double s;
+  Color color;
+  TextDirection textDirection;
 
-typedef _LetIndexPage = bool Function(int value);
+  NavCustomPainter(
+      double startingLoc, int itemsLength, this.color, this.textDirection) {
+    final span = 1.0 / itemsLength;
+    s = 0.2;
+    double l = startingLoc + (span - s) / 2;
+    loc = textDirection == TextDirection.rtl ? 0.8 - l : l;
+  }
 
-class CurvedNavigationBar extends StatefulWidget {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+
+    final path = Path()
+      ..moveTo(0, 0)
+      ..lineTo((loc - 0.1) * size.width, 0)
+      ..cubicTo(
+        (loc + s * 0.20) * size.width,
+        size.height * 0.05,
+        loc * size.width,
+        size.height * 0.60,
+        (loc + s * 0.50) * size.width,
+        size.height * 0.60,
+      )
+      ..cubicTo(
+        (loc + s) * size.width,
+        size.height * 0.60,
+        (loc + s - s * 0.20) * size.width,
+        size.height * 0.05,
+        (loc + s + 0.1) * size.width,
+        0,
+      )
+      ..lineTo(size.width, 0)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return this != oldDelegate;
+  }
+}
+
+class NavCustomClipper extends CustomClipper<Path> {
+  final double deviceHeight;
+
+  NavCustomClipper({required this.deviceHeight});
+
+  @override
+  Path getClip(Size size) {
+    final path = Path()
+      ..moveTo(0, 0)
+      ..lineTo(size.width, 0)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..close();
+    // Adjust the path to match your curved design
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class BlurredNavBar extends StatelessWidget {
   final List<Widget> items;
-  final int index;
   final Color color;
-  final Color? buttonBackgroundColor;
   final Color backgroundColor;
-  final ValueChanged<int>? onTap;
-  final _LetIndexPage letIndexChange;
-  final Curve animationCurve;
-  final Duration animationDuration;
+  final TextDirection textDirection;
   final double height;
   final double? maxWidth;
 
-  CurvedNavigationBar({
-    Key? key,
+  BlurredNavBar({
     required this.items,
-    this.index = 0,
     this.color = Colors.white,
-    this.buttonBackgroundColor,
     this.backgroundColor = Colors.blueAccent,
-    this.onTap,
-    _LetIndexPage? letIndexChange,
-    this.animationCurve = Curves.easeOut,
-    this.animationDuration = const Duration(milliseconds: 600),
+    this.textDirection = TextDirection.ltr,
     this.height = 75.0,
     this.maxWidth,
-  })  : letIndexChange = letIndexChange ?? ((_) => true),
-        assert(items.isNotEmpty),
-        assert(0 <= index && index < items.length),
-        assert(0 <= height && height <= 75.0),
-        assert(maxWidth == null || 0 <= maxWidth),
-        super(key: key);
-
-  @override
-  CurvedNavigationBarState createState() => CurvedNavigationBarState();
-}
-
-class CurvedNavigationBarState extends State<CurvedNavigationBar>
-    with SingleTickerProviderStateMixin {
-  late double _startingPos;
-  late int _endingIndex;
-  late double _pos;
-  double _buttonHide = 0;
-  late Widget _icon;
-  late AnimationController _animationController;
-  late int _length;
-
-  @override
-  void initState() {
-    super.initState();
-    _icon = widget.items[widget.index];
-    _length = widget.items.length;
-    _pos = widget.index / _length;
-    _startingPos = widget.index / _length;
-    _endingIndex = widget.index;
-    _animationController = AnimationController(vsync: this, value: _pos);
-    _animationController.addListener(() {
-      setState(() {
-        _pos = _animationController.value;
-        final endingPos = _endingIndex / widget.items.length;
-        final middle = (endingPos + _startingPos) / 2;
-        if ((endingPos - _pos).abs() < (_startingPos - _pos).abs()) {
-          _icon = widget.items[_endingIndex];
-        }
-        _buttonHide =
-            (1 - ((middle - _pos) / (_startingPos - middle)).abs()).abs();
-      });
-    });
-  }
-
-  @override
-  void didUpdateWidget(CurvedNavigationBar oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.index != widget.index) {
-      final newPosition = widget.index / _length;
-      _startingPos = _pos;
-      _endingIndex = widget.index;
-      _animationController.animateTo(newPosition,
-          duration: widget.animationDuration, curve: widget.animationCurve);
-    }
-    if (!_animationController.isAnimating) {
-      _icon = widget.items[_endingIndex];
-    }
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
+  });
 
   @override
   Widget build(BuildContext context) {
-    final textDirection = Directionality.of(context);
     return SizedBox(
-      height: widget.height,
+      height: height,
       child: LayoutBuilder(
         builder: (context, constraints) {
-          final maxWidth = min(
-              constraints.maxWidth, widget.maxWidth ?? constraints.maxWidth);
-          return Align(
-            alignment: textDirection == TextDirection.ltr
-                ? Alignment.bottomLeft
-                : Alignment.bottomRight,
-            child: Stack(
-              children: [
-                // BackdropFilter for the background only
-                Positioned.fill(
+          final maxwidth =
+              min(constraints.maxWidth, maxWidth ?? constraints.maxWidth);
+          return Stack(
+            children: [
+              // Custom ClipPath to restrict the blur to the curved shape
+              ClipPath(
+                clipper: NavCustomClipper(
+                  deviceHeight: MediaQuery.sizeOf(context).height,
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
                   child: Container(
-                    color: widget.backgroundColor.withOpacity(0.2),
+                    color: backgroundColor.withOpacity(0.2),
+                    width: maxWidth,
+                    height: height,
                   ),
                 ),
-                // The rest of the navigation bar
-                Container(
-                  width: maxWidth,
-                  child: ClipRect(
-                    clipper: NavCustomClipper(
-                      deviceHeight: MediaQuery.sizeOf(context).height,
-                    ),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        alignment: Alignment.bottomCenter,
-                        children: <Widget>[
-                          Positioned(
-                            bottom: -40 - (75.0 - widget.height),
-                            left: textDirection == TextDirection.rtl
-                                ? null
-                                : _pos * maxWidth,
-                            right: textDirection == TextDirection.rtl
-                                ? _pos * maxWidth
-                                : null,
-                            width: maxWidth / _length,
-                            child: Center(
-                              child: Transform.translate(
-                                offset: Offset(
-                                  0,
-                                  -(1 - _buttonHide) * 80,
-                                ),
-                                child: Material(
-                                  color: widget.buttonBackgroundColor ??
-                                      widget.color,
-                                  type: MaterialType.circle,
-                                  elevation: 8.0, // Optional: Shadow for button
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: _icon, // Only the icon
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            left: 0,
-                            right: 0,
-                            bottom: 0 - (75.0 - widget.height),
-                            child: CustomPaint(
-                              painter: NavCustomPainter(
-                                  _pos, _length, widget.color, textDirection),
-                              child: Container(
-                                height: 75.0,
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            left: 0,
-                            right: 0,
-                            bottom: 0 - (75.0 - widget.height),
-                            child: SizedBox(
-                              height: 100.0,
-                              child: Row(
-                                children: widget.items.map((item) {
-                                  return NavButton(
-                                    onTap: _buttonTap,
-                                    position: _pos,
-                                    length: _length,
-                                    index: widget.items.indexOf(item),
-                                    child: Center(child: item),
-                                  );
-                                }).toList(),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+              ),
+              // The CustomPaint with the curved shape
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: CustomPaint(
+                  painter: NavCustomPainter(
+                    0.5, // Example startingLoc value
+                    items.length,
+                    color,
+                    textDirection,
                   ),
+                  size: Size(maxwidth, height),
                 ),
-              ],
-            ),
+              ),
+              // Add other widgets here, such as your nav items
+            ],
           );
         },
       ),
     );
-  }
-
-  void setPage(int index) {
-    _buttonTap(index);
-  }
-
-  void _buttonTap(int index) {
-    if (!widget.letIndexChange(index) || _animationController.isAnimating) {
-      return;
-    }
-    if (widget.onTap != null) {
-      widget.onTap!(index);
-    }
-    final newPosition = index / _length;
-    setState(() {
-      _startingPos = _pos;
-      _endingIndex = index;
-      _animationController.animateTo(newPosition,
-          duration: widget.animationDuration, curve: widget.animationCurve);
-    });
   }
 }
